cffe409d037b8ee3c546601cabf0e785d06e364d
tobi <tobi@backfrak.de>
2019-10-28 19:03:01 +0100

    Change the author to the mail address

--- src/tobi.backfrak.de/internal/testhelper/PathHelper.go
+++ src/tobi.backfrak.de/internal/testhelper/PathHelper.go
@@ -1,6 +1,6 @@
 package testhelper
 
-// Copyright 2019 by Tobias Zellner. All
+// Copyright 2019 by tobi@backfrak.de. All
 // rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the
 // LICENSE file.
