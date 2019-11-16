8c9382e940fda6804a5232a9b120932a3b337ca0
tobi <tobi@backfrak.de>
2019-11-16 16:19:06 +0100

    Use the interfaces in the main function

--- src/tobi.backfrak.de/cmd/gpsa/main.go
+++ src/tobi.backfrak.de/cmd/gpsa/main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"os"
 
+	"tobi.backfrak.de/internal/gpsabl"
 	"tobi.backfrak.de/internal/gpxbl"
 )
 
@@ -18,18 +19,33 @@ func main() {
 		fmt.Println("Argument given:")
 		for _, arg := range os.Args[1:] { // Skip the 0s argument, becaus this will always be the program itselfe
 			fmt.Println("Read file: " + arg)
+			// Get the GpxFile type
 			gpx := gpxbl.NewGpxFile(arg)
-			file, err := gpx.ReadTracks()
+
+			// Convert the GpxFile to the TrackReader interface
+			reader := gpsabl.TrackReader(&gpx)
+
+			// Read the *.gpx into a TrackFile type, using the interface
+			file, err := reader.ReadTracks()
+
 			if err != nil {
 				HandleError(err, arg)
 			}
+			// Convert the TrackFile into the TrackInfoProvider interface
+			info := gpsabl.TrackInfoProvider(file)
+
+			// Read Properties from the TrackFile
 			fmt.Println("Name: ", file.Name)
 			fmt.Println("Description: ", file.Description)
-			fmt.Println("NumberOfTracks: ", file.NumberOfTracks)
-			fmt.Println("Distance: ", file.Distance)
-			fmt.Println("AtituteRange: ", file.AtituteRange)
-			fmt.Println("MinimumAtitute: ", file.MinimumAtitute)
-			fmt.Println("MaximumAtitute: ", file.MaximumAtitute)
+
+			// Read Properties from the GpxFile
+			fmt.Println("NumberOfTracks: ", gpx.NumberOfTracks)
+
+			// Read properties troutgh the interface
+			fmt.Println("Distance: ", info.GetDistance())
+			fmt.Println("AtituteRange: ", info.GetAtituteRange())
+			fmt.Println("MinimumAtitute: ", info.GetMinimumAtitute())
+			fmt.Println("MaximumAtitute: ", info.GetMaximumAtitute())
 		}
 	}
 
