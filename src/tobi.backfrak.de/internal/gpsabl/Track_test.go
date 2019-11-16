bba06d9218bf6713380ea9ef206cad88229dd577
tobi <tobi@backfrak.de>
2019-11-16 13:21:01 +0100

    More test

--- src/tobi.backfrak.de/internal/gpsabl/Track_test.go
+++ src/tobi.backfrak.de/internal/gpsabl/Track_test.go
@@ -10,6 +10,31 @@ func TestNewTrackFile(t *testing.T) {
 	}
 }
 
+func TestTrackFileIsTrackSummary(t *testing.T) {
+	file := TrackFile{}
+	sum := TrackInfoProvider(file)
+
+	if sum == nil {
+		t.Errorf("The Track struct does not implement the TrackInfoProvider interface as expected")
+	}
+
+	if sum.GetDistance() != 0.0 {
+		t.Errorf("The GetDistance() does return %f, but %f was expected", sum.GetDistance(), 0.0)
+	}
+
+	if sum.GetAtituteRange() != 0.0 {
+		t.Errorf("The GetAtituteRange() does return %f, but %f was expected", sum.GetAtituteRange(), 0.0)
+	}
+
+	if sum.GetMaximumAtitute() != 0.0 {
+		t.Errorf("The GetMaximumAtitute() does return %f, but %f was expected", sum.GetMaximumAtitute(), 0.0)
+	}
+
+	if sum.GetMinimumAtitute() != 0.0 {
+		t.Errorf("The GetMinimumAtitute() does return %f, but %f was expected", sum.GetMinimumAtitute(), 0.0)
+	}
+}
+
 func TestTrackIsTrackSummary(t *testing.T) {
 	trk := Track{}
 	sum := TrackInfoProvider(trk)
