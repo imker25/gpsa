0a11900a082a303a9a40690b99082009c6645925
tobi <tobi@backfrak.de>
2020-05-27 19:45:25 +0200

    Next step

--- build/PrepareRelease.sh
+++ build/PrepareRelease.sh
@@ -16,6 +16,7 @@ function print_usage()  {
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BRANCH_ROOT="$SCRIPT_DIR/.."
 LOG_DIR="$BRANCH_ROOT/logs"
+RELEASE_BASE_BRANCH="master"
 
 # ################################################################################################################
 # functional code
@@ -29,14 +30,20 @@ fi
 pushd "$BRANCH_ROOT"
 
 actualBranch=$(git status -b -s)
-statusLines=$(echo "$actualBranch" | wc -l)
-if [ "$statusLines" != "1" ]; then
-    echo "Error: There are change files that are not checked in."
+if [ $? != 0 ]; then
+    echo "Error: Can not get branch information"
     popd
     exit -1
 fi 
 
-echo "Branch Status has $statusLines lines"
+statusLines=$(echo "$actualBranch" | wc -l)
+# if [ "$statusLines" != "1" ]; then
+#     echo "Error: There are change files that are not checked in."
+#     popd
+#     exit -1
+# fi 
+
+echo "Branch Status: \"$actualBranch\" "
 
 popd
 exit 0
