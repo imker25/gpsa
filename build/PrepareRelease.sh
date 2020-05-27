ebf535cefda7db1bfa553133be42427f9bb731c8
tobi <tobi@backfrak.de>
2020-05-27 19:32:09 +0200

    Start with the scripted realease

---
+++ build/PrepareRelease.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ################################################################################################################
+# function definition
+# ################################################################################################################
+function print_usage()  {
+    echo "Usage: $0 options"
+    echo "-help    Print this help"
+    echo "This script will do all needed steps to do a new gpsa release localy."
+    echo "And ask in the end if the result should be published on GutHub"
+}
+
+# ################################################################################################################
+# variable asigenment
+# ################################################################################################################
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BRANCH_ROOT="$SCRIPT_DIR/.."
+LOG_DIR="$BRANCH_ROOT/logs"
+
+# ################################################################################################################
+# functional code
+# ################################################################################################################
+
+if [ "$1" == "-help" ]; then
+    print_usage
+    exit 0
+fi  
+
+pushd "$BRANCH_ROOT"
+
+
+
+popd
+exit 0
